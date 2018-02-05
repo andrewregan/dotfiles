8eaed34e1a8cdedeb0723641f4da74377f6ddfc9
James Warp <james@jnwarp.com>
2018-02-05 09:48:38 -0500

    Separate setup script to file

---
+++ script/i3-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo cp ~/.dotfiles/i3lock-fancy/lock /usr/local/bin
+sudo cp ~/.dotfiles/i3lock-fancy/icons /usr/local/bin/icons -r
+sudo chmod a+rw/sys/class/backlight/intel_backlight/brightness
