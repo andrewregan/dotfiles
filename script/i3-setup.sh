dc01a93b24c6f82a1fa31e2f79900b892fbf8c2a
James Warp <james@jnwarp.com>
2018-02-11 18:31:06 -0500

    Add background to i3

--- script/i3-setup.sh
+++ script/i3-setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# install i3wm
+sudo apt install i3 i3blocks
+
+# install i3lock
 sudo cp ~/.dotfiles/i3lock-fancy/lock /usr/local/bin
 sudo cp ~/.dotfiles/i3lock-fancy/icons /usr/local/bin/icons -r
+
+# make backlight writable
 sudo chmod a+rw/sys/class/backlight/intel_backlight/brightness
