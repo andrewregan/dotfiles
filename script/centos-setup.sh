c50bae8d83ac458408ed2eaebc69ec97ce71551f
James Warp <james@jnwarp.com>
2020-01-08 12:48:55 -0500

    Create centos-setup.sh

---
+++ script/centos-setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# disable root - AFTER LOGIN!
+if [ $EUID == 0 ]; then
+  echo Root account not disabled.
+  echo Please run this script as a regular user!
+  exit
+else
+  sudo passwd -l root
+  echo Root account disabled!
+fi
+
+# install tools
+sudo dnf update
+sudo dnf upgrade -y
+sudo dnf install git curl vim zsh -y
+
+# install additional tools
+sudo dnf install epel-release -y
+sudo dnf update -y
+sudo dnf install htop -y
+sudo dnf install neofetch -y
+
+# install system changing items
+sudo dnf install fail2ban -y
+
+# clone dotfiles
+git clone https://github.com/jnwarp/dotfiles ~/.dotfiles
+
+# dotbot install
+~/.dotfiles/install
+
+# done
+echo Setup is complete, please log in as the new user and run the commands below!
+echo '~/.dotfiles/install; ssh-keygen -t rsa -b 4096 -C "james@jnwarp.com `hostname`"; cat ~/.ssh/id_rsa.pub'
